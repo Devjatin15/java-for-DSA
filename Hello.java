7709c6dfd58fbf79c2cbf56ab96ca128d8e86386
Devjatin15 <jatinkumar1562006@gmail.com>
2026-03-29 14:56:35 +0000

    day 1 of java in 100 days

---
+++ Hello.java
@@ -0,0 +1,12 @@
+// we can test out java by opening jshell in command prompt
+// java is a write once and read anywhere language 
+
+
+class Hello
+
+{
+    public static void main(String a[])
+    {
+        System.out.println("Hello World");
+    }
+}
